ac8d73b85c947d7315237c5cece4f54b42477a43
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-21 15:34:54 +0000

    script to run all 5 toro tests

---
+++ toro.sh
@@ -0,0 +1,17 @@
+set -e
+
+parallel 'python toro.py -x 1000 -o toro{}.h5 -c {}' ::: {1..5}
+pypy run.py -on 100 -t 0.3   toro1.h5 &
+pypy run.py -on 100 -t 0.14  toro2.h5 &
+pypy run.py -on 100 -t 0.012 toro3.h5 &
+pypy run.py -on 100 -t 0.05  toro4.h5 &
+pypy run.py -on 100 -t 0.012 toro5.h5 &
+wait
+
+parallel --bar '
+  python plot_1d.py toro{1}.h5 {2} -o toro{1}_{2}.pdf
+' ::: {1..5} ::: density pressure velocity_x
+parallel --bar '
+  python plot_xt.py toro{1}.h5 density -o toro{1}_xt.pdf
+' ::: {1..5}
+pdfjoin toro?_*.pdf -o toro.pdf
