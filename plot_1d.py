e78559968ad9d761d5895cd67a141746fd17bef8
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-23 12:32:05 +0000

    make plot_1d able to plot more than one variable

--- plot_1d.py
+++ plot_1d.py
@@ -10,7 +10,7 @@ from euler import Euler
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("file", help="file to read", type=str)
-parser.add_argument("variable", help="variable to plot", type=str)
+parser.add_argument("variable", help="variable to plot", type=str, nargs='+')
 parser.add_argument("-i", help="step number to plot", default=-1, type=int)
 parser.add_argument("-o", help="output file")
 args = parser.parse_args()
@@ -20,10 +20,9 @@ sys = np.loads(f.attrs['system'])
 D = len(f['h'])
 
 x = [f[n] for (n,i) in zip("xyz",range(0,D))]
-q = getattr(sys, args.variable)(f['q'][args.i])
 
-plt.plot(x[0], q, '.-')
-plt.title("%s @ %d:%f" % (args.file, f['i'][args.i], f['t'][args.i]))
+plt.plot(*sum(([x[0], getattr(sys,v)(f['q'][args.i]), '.-'] for v in args.variable), []))
+plt.title("%s @ %d:%f" % (args.file, *[f[n][args.i] for n in "it"]))
 plt.ylabel("%s" % args.variable)
 
 if args.o:
