553821bcdddbde7e8838ffe8d1ce082ca05c4069
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-23 11:24:57 +0000

    complete conversion of solvers to class methods

--- solver.py
+++ solver.py
@@ -19,28 +19,28 @@ class ConservationLaw:
 
         return q - dt*sum(np.diff(flux(i), axis=i+1)/h[i] for i in range(0,D))
 
-    def llf(sys, D, ql, qr, fl, fr, dx, dt):
+    def llf(self, D, ql, qr, fl, fr, dx, dt):
         lam = D*dt/dx
         return (fr + fl - (qr - ql)/lam)/2
 
-    def force(sys, D, ql, qr, fl, fr, dx, dt, i):
+    def force(self, D, ql, qr, fl, fr, dx, dt, i):
         lam = D*dt/dx
-        lf =       (fr + fl - (qr - ql)/lam)/2
-        lw = sys.F((qr + ql - (fr - fl)*lam)/2)[i]
+        lf =        (fr + fl - (qr - ql)/lam)/2
+        lw = self.F((qr + ql - (fr - fl)*lam)/2)[i]
         return (lf + lw)/2
 
 
 class ModifiedConservationLaw(ConservationLaw):
     def update(self, D, h, dt, q):
         Q = np.pad(q, (((0,0),) + ((1,1),)*D), 'edge')
-        F = sys.F(Q)
-        H = sys.H(Q)
+        F = self.F(Q)
+        H = self.H(Q)
 
         def flux(i):
             lidx = (slice(None),) + tuple((slice(None, -1) if j == i else slice(1,-1)) for j in range(0,D))
             ridx = (slice(None),) + tuple((slice( 1, None) if j == i else slice(1,-1)) for j in range(0,D))
 
-            return force(D, sys, Q[lidx], Q[ridx], F[(i,)+lidx], F[(i,)+ridx], h[i], dt, i)
+            return self.force(D, Q[lidx], Q[ridx], F[(i,)+lidx], F[(i,)+ridx], h[i], dt, i)
 
         return q - dt*sum(np.diff(flux(i), axis=i+1)/h[i] for i in range(0,D))
 
