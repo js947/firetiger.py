5aa652d77c0b78807e33fb30a3d1ae81e779ed4d
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-28 16:27:37 +0000

    refactor solver module

--- solver.py
+++ solver.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-def lidx(i, D): return (slice(None),) + tuple((slice(None, -1) if j == i else slice(1,-1)) for j in range(0,D))
-def ridx(i, D): return (slice(None),) + tuple((slice( 1, None) if j == i else slice(1,-1)) for j in range(0,D))
-
 class ConservationLaw:
     def cfl(self, q, h, cfl=0.95):
         D = len(h)
@@ -29,6 +26,7 @@ class ConservationLaw:
 
         return q - dt*sum(np.diff(flux(i), axis=i+1)/h[i] for i in range(0,D))
 
+
 class ModifiedConservationLaw(ConservationLaw):
     def update(self, D, h, dt, q):
         Q = np.pad(q, (((0,0),) + ((1,1),)*D), 'edge')
@@ -45,4 +43,6 @@ class ModifiedConservationLaw(ConservationLaw):
 
         return q - dt*sum(np.diff(flux(i), axis=i+1)/h[i] for i in range(0,D))
 
+def lidx(i, D): return (slice(None),) + tuple((slice(None, -1) if j == i else slice(1,-1)) for j in range(0,D))
+def ridx(i, D): return (slice(None),) + tuple((slice( 1, None) if j == i else slice(1,-1)) for j in range(0,D))
 
