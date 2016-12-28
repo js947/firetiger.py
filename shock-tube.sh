3872e007883b723266345c96e0c4151baec0aa3f
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-28 16:02:04 +0000

    use executables directly in toro test script

--- shock-tube.sh
+++ shock-tube.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-parallel 'python shock-tube.py -x 1000 -o shock-tube{}.h5 -c {}' ::: {1..5}
-python run.py -on 100 -t 0.3   shock-tube.h5 &
-python run.py -on 100 -t 0.14  shock-tube.h5 &
-python run.py -on 100 -t 0.012 shock-tube.h5 &
-python run.py -on 100 -t 0.05  shock-tube.h5 &
-python run.py -on 100 -t 0.012 shock-tube.h5 &
+parallel './shock-tube.py -x 1000 -o shock-tube{}.h5 -c {}' ::: {1..5}
+./run.py -on 100 -t 0.3   shock-tube1.h5 &
+./run.py -on 100 -t 0.14  shock-tube2.h5 &
+./run.py -on 100 -t 0.012 shock-tube3.h5 &
+./run.py -on 100 -t 0.05  shock-tube4.h5 &
+./run.py -on 100 -t 0.012 shock-tube5.h5 &
 wait
 
 parallel --bar '
-  python plot_1d.py shock-tube{1}.h5 {2} -o shock-tube{1}_{2}.pdf
+  ./plot_1d.py shock-tube{1}.h5 {2} -o shock-tube{1}_{2}.pdf
 ' ::: {1..5} ::: density pressure velocity_x
 parallel --bar '
-  python plot_xt.py shock-tube{1}.h5 density -o shock-tube{1}_xt.pdf
+  ./plot_xt.py shock-tube{1}.h5 density -o shock-tube{1}_xt.pdf
 ' ::: {1..5}
 pdfjoin shock-tube?_*.pdf -o shock-tube.pdf
