64d80e9e12a77779e6e2795290c88d55fda3ee10
Jeffrey Salmond <js947@cam.ac.uk>
2016-12-29 19:05:33 +0000

    make shock-tube test case able to generate 1, 2 and 3 dimensional problems

--- shock-tube.py
+++ shock-tube.py
@@ -11,6 +11,8 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument("-x", help="x resolution", default=200, type=int)
 parser.add_argument("-c", help="test case", default=1, type=int)
 parser.add_argument("-o", help="simulation file", default="shock-tube.h5", type=str)
+parser.add_argument("-D", help="dimensions",default=1, type=int)
+parser.add_argument("-d", help="dimension in which to make the shock", default='x', choices='xyz', type=str)
 args = parser.parse_args()
 
 sys = Euler(IdealGas(1.4))
@@ -20,28 +22,35 @@ def coord(x0, x1, nx):
     x += dx/2
     return x[:-1], dx
 
-x, dx = coord(0.0, 1.0, args.x)
+def vd(v):
+    return [v if i == args.d else 0.0 for i in range(0,args.D)]
 
 m, qL, qR = {
-    0: (0.3, sys.cons(1.0, 1.0, 0.0), sys.cons(0.125, 0.1, 0.0)),
-    1: (0.3, sys.cons(1.0, 1.0, 0.75), sys.cons(0.125, 0.1, 0.0)),
-    2: (0.5, sys.cons(1.0, 0.4, -2.0), sys.cons(1.0,0.4,2.0)),
-    3: (0.5, sys.cons(1.0, 1000.0, 0.0), sys.cons(1.0,0.01,0.0)),
-    4: (0.3, sys.cons(5.99924, 460.894, 19.5975), sys.cons(5.99242, 46.095, -6.19633)),
-    5: (0.8, sys.cons(1.0,1000.0,-19.59745), sys.cons(1.0,0.01,-19.59745)),
+    0: (0.3, sys.cons(    1.0,     1.0, *vd(     0.0)), sys.cons(  0.125,    0.1, *vd(      0.0))),
+    1: (0.3, sys.cons(    1.0,     1.0, *vd(    0.75)), sys.cons(  0.125,    0.1, *vd(      0.0))),
+    2: (0.5, sys.cons(    1.0,     0.4, *vd(    -2.0)), sys.cons(    1.0,    0.4, *vd(      2.0))),
+    3: (0.5, sys.cons(    1.0,  1000.0, *vd(     0.0)), sys.cons(    1.0,   0.01, *vd(      0.0))),
+    4: (0.3, sys.cons(5.99924, 460.894, *vd( 19.5975)), sys.cons(5.99242, 46.095, *vd( -6.19633))),
+    5: (0.8, sys.cons(    1.0,  1000.0, *vd(-19.5975)), sys.cons(    1.0,   0.01, *vd(-19.59745))),
 }[args.c]
 
-q = np.where(x < m, qL[:,None], qR[:,None])
+xdx = [coord(0.0, 1.0, args.x) for i in range(0,args.D)]
+x = np.meshgrid(*[j[0] for j in xdx])
+
+idx = (slice(None),) + (None,)*args.D
+q = np.where(x[{'x':0, 'y':1, 'z':2}[args.d]] < m, qL[idx], qR[idx])
 
 f = h5py.File(args.o, 'w')
 f.attrs['system'] = np.string_(pickle.dumps(sys))
-h_ = f.create_dataset('h', data=[dx])
-x_ = f.create_dataset('x', data=x);
+
+h_ = f.create_dataset('h', data=[j[1] for j in xdx])
+for n, d in zip('xyz', x):
+    f.create_dataset(n, data=d)
 
 i_ = f.create_dataset('i', (1,), maxshape=(None,), dtype='i')
 t_ = f.create_dataset('t', (1,), maxshape=(None,), dtype='f')
 q_ = f.create_dataset('q', (1,)+q.shape, maxshape=(None,)+q.shape, dtype='f', compression="lzf")
 
-t_[0] = 0.0
 i_[0] = 0
+t_[0] = 0.0
 q_[0] = q
