85741f901943a2cfba54229e1bd718ad19b5f193
Jeffrey Salmond <js947@cam.ac.uk>
2017-01-02 17:03:07 +0000

    add temperature constants and function to ideal and stiffened gases

--- eos.py
+++ eos.py
@@ -1,22 +1,21 @@
 import numpy as np
 
 class IdealGas:
-    def __init__(self, gamma):
-        self.gamma = gamma
+    def __init__(self, gamma, cv=1, q=0):
+        self.gamma, self.cv, self.q = gamma, cv, q
 
     def energy(self, v, p):
-        return p*v/(self.gamma - 1)
+        return p*v/(self.gamma - 1) + self.q
     def pressure(self, v, e):
-        return e*(self.gamma - 1)/v
+        return (e - self.q)*(self.gamma - 1)/v
     def soundspd(self, v, p):
         return np.sqrt(v*p*self.gamma)
-
-    def __repr__(self):
-        return "eos.IdealGas(%f)" % self.gamma
+    def temperature(self, v, e):
+        return (e - self.q)/cv
 
 class StiffenedGas:
-    def __init__(self, gamma, p0):
-        self.gamma, self.p0 = gamma, p0
+    def __init__(self, gamma, p0, cv=1, q=0):
+        self.gamma, self.p0, self.cv, self.q = gamma, p0, cv, q
 
     def energy(self, v, p):
         return (p + self.gamma*self.p0)*v/(self.gamma - 1)
@@ -24,6 +23,6 @@ class StiffenedGas:
         return e*(self.gamma - 1.0)/v - self.gamma*self.p0
     def soundspd(self, v, p):
         return np.sqrt(v*(p + self.p0)*self.gamma)
+    def temperature(self, v, e):
+        return (e - self.q)/cv
 
-    def __repr__(self):
-        return "eos.StiffenedGas(%f,%f)" % (self.gamma, self.p0)
